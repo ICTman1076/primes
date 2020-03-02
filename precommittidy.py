40d62acb8f0cf00214a78c5a2100d02c1cf64547
ICTman1076 <ict@stch.tk>
2020-03-02 12:14:30 +0000

    New primes!

--- precommittidy.py
+++ precommittidy.py
@@ -2,5 +2,8 @@ data=[]
 import json
 with open("./primessource/primes.txt") as f:
     data=json.loads(f.read())
+newData=[]
+for i in data:
+    newData.append(str(i))
 with open("./primessource/primes.txt","w") as f:
-    f.write("\n".join(data))
+    f.write("\n".join(newData))
