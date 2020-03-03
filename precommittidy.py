c85c7f47cc807787874b2d17383aba846849912b
ICTman1076 <ict@stch.tk>
2020-03-03 09:51:08 +0000

    More output in tidy file

--- precommittidy.py
+++ precommittidy.py
@@ -1,9 +1,18 @@
+print("Preparing...")
+import time
 data=[]
 import json
+timethen=time.time()
+print("Loading in data...")
 with open("./primessource/primes.txt") as f:
     data=json.loads(f.read())
 newData=[]
+print("Converting data...")
 for i in data:
     newData.append(str(i))
+print("Making a string...")
+finalContent="\n".join(newData)
+print("...and now writing it in.")
 with open("./primessource/primes.txt","w") as f:
-    f.write("\n".join(newData))
+    f.write()
+print("Done in "+str(int((time.time()-timethen)*10)/10)+"s")
